217ef0b5fd5fcb7cdd3dfb1b54d4bb57a123e37d
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-05-27 18:58:52 -0300

    commit inicial

---
+++ Stepper/Stepper/Docente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepper
+{
+    class Docente : Pessoa
+    {
+        public String Usuario { set; get; }
+        public String Senha { set; get; }
+
+        public Docente() { }
+        public Boolean Login()
+        {
+            return false;
+        }
+    }
+}
---
+++ Stepper/Stepper/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepper
+{
+    class Pessoa
+    {
+        public Int32 Id { set; get; }
+        public String Nome { set; get; }
+
+        public Pessoa() { }
+    }
+}
