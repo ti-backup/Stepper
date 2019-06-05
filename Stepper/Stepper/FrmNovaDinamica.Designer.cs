eeee6d7e40120417d7e528eb47975927676eb0e1
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-06-04 22:01:02 -0300

    Desenvolvimento do recurso para importar participantes a partir de um arquivo Excel

--- Stepper/Stepper/FrmNovaDinamica.Designer.cs
+++ Stepper/Stepper/FrmNovaDinamica.Designer.cs
@@ -28,17 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtArquivo = new System.Windows.Forms.TextBox();
-            this.txtNome = new System.Windows.Forms.TextBox();
-            this.btnProcurar = new System.Windows.Forms.Button();
-            this.btnAdicionar = new System.Windows.Forms.Button();
             this.dgvParticipantes = new System.Windows.Forms.DataGridView();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.ofdArquivo = new System.Windows.Forms.OpenFileDialog();
+            this.participanteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.observacaoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvParticipantes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.participanteBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -57,48 +63,22 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Adicionar Participantes";
             // 
-            // label1
+            // dgvParticipantes
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Arquivo:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 65);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Nome:";
-            // 
-            // txtArquivo
-            // 
-            this.txtArquivo.Location = new System.Drawing.Point(6, 32);
-            this.txtArquivo.Name = "txtArquivo";
-            this.txtArquivo.ReadOnly = true;
-            this.txtArquivo.Size = new System.Drawing.Size(100, 20);
-            this.txtArquivo.TabIndex = 2;
-            // 
-            // txtNome
-            // 
-            this.txtNome.Location = new System.Drawing.Point(6, 81);
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 20);
-            this.txtNome.TabIndex = 3;
-            // 
-            // btnProcurar
-            // 
-            this.btnProcurar.Location = new System.Drawing.Point(112, 30);
-            this.btnProcurar.Name = "btnProcurar";
-            this.btnProcurar.Size = new System.Drawing.Size(82, 23);
-            this.btnProcurar.TabIndex = 4;
-            this.btnProcurar.Text = "Procurar...";
-            this.btnProcurar.UseVisualStyleBackColor = true;
-            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            this.dgvParticipantes.AllowUserToAddRows = false;
+            this.dgvParticipantes.AllowUserToDeleteRows = false;
+            this.dgvParticipantes.AutoGenerateColumns = false;
+            this.dgvParticipantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParticipantes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nomeDataGridViewTextBoxColumn,
+            this.observacaoDataGridViewTextBoxColumn});
+            this.dgvParticipantes.DataSource = this.participanteBindingSource;
+            this.dgvParticipantes.Location = new System.Drawing.Point(6, 107);
+            this.dgvParticipantes.Name = "dgvParticipantes";
+            this.dgvParticipantes.ReadOnly = true;
+            this.dgvParticipantes.Size = new System.Drawing.Size(188, 129);
+            this.dgvParticipantes.TabIndex = 1;
             // 
             // btnAdicionar
             // 
@@ -110,20 +90,78 @@
             this.btnAdicionar.UseVisualStyleBackColor = true;
             this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
             // 
-            // dgvParticipantes
+            // btnProcurar
             // 
-            this.dgvParticipantes.AllowUserToAddRows = false;
-            this.dgvParticipantes.AllowUserToDeleteRows = false;
-            this.dgvParticipantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvParticipantes.Location = new System.Drawing.Point(6, 107);
-            this.dgvParticipantes.Name = "dgvParticipantes";
-            this.dgvParticipantes.ReadOnly = true;
-            this.dgvParticipantes.Size = new System.Drawing.Size(188, 129);
-            this.dgvParticipantes.TabIndex = 1;
+            this.btnProcurar.Location = new System.Drawing.Point(112, 30);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(82, 23);
+            this.btnProcurar.TabIndex = 4;
+            this.btnProcurar.Text = "Procurar...";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(6, 81);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Location = new System.Drawing.Point(6, 32);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.ReadOnly = true;
+            this.txtArquivo.Size = new System.Drawing.Size(100, 20);
+            this.txtArquivo.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arquivo:";
             // 
             // ofdArquivo
             // 
             this.ofdArquivo.FileName = "openFileDialog1";
+            // 
+            // participanteBindingSource
+            // 
+            this.participanteBindingSource.DataSource = typeof(Stepper.Participante);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            this.nomeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // observacaoDataGridViewTextBoxColumn
+            // 
+            this.observacaoDataGridViewTextBoxColumn.DataPropertyName = "Observacao";
+            this.observacaoDataGridViewTextBoxColumn.HeaderText = "Observação";
+            this.observacaoDataGridViewTextBoxColumn.Name = "observacaoDataGridViewTextBoxColumn";
+            this.observacaoDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // FrmNovaDinamica
             // 
@@ -136,6 +174,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvParticipantes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.participanteBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -151,5 +190,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvParticipantes;
         private System.Windows.Forms.OpenFileDialog ofdArquivo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn observacaoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource participanteBindingSource;
     }
 }
--- Stepper/Stepper/FrmNovaDinamica.cs
+++ Stepper/Stepper/FrmNovaDinamica.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Stepper
 {
@@ -46,12 +48,45 @@ namespace Stepper
                 caminho = ofdArquivo.FileName.Split('\\');
                 txtArquivo.Text = caminho[caminho.Length - 1];
                 LeArquivo(ofdArquivo.FileName);
+                AtualizaViewParticipantes();
             }
         }
 
         private void LeArquivo(String caminhoArquivo)
         {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(caminhoArquivo);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
 
+            Byte numColunas = 1;
+            Byte numLinhas = 7;
+
+            for (Byte i = 1; i <= numColunas; i++)
+            {
+                for (Byte j = 2; j <= numLinhas; j++)
+                {
+                    if (xlRange.Cells[j, i] != null && xlRange.Cells[j, i].Value2 != null)
+                    {
+                        Participante p = new Participante();
+                        p.Nome = xlRange.Cells[j, i].Value2.ToString();
+
+                        Participantes.Add(p);
+                    }
+                }
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
         }
     }
 }
